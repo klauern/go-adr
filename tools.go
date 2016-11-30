e529a2d4e4ddaf3b46118608013259ec570af9c2
Nick Klauer <klauer@gmail.com>
2016-11-30 10:30:45 -0600

    add more stuff

---
+++ tools.go
@@ -0,0 +1,38 @@
+package adr
+
+import "text/template"
+import "time"
+import "io/ioutil"
+
+import "bufio"
+import "bytes"
+
+// DecisionRecord is a type of Architecture Decision Record.
+type DecisionRecord struct {
+	Number       int
+	Title        string
+	Date         time.Time
+	Status       string
+	Context      string
+	Decision     string
+	Consequences string
+}
+
+func (d DecisionRecord) format() (string, error) {
+	tmpl, err := ioutil.ReadFile("templates/template.md")
+	if err != nil {
+		return "", err
+	}
+	initial, err := template.New("initial").Parse(string(tmpl))
+	if err != nil {
+		return "", err
+	}
+
+	var wr bytes.Buffer
+	fmtwr := bufio.NewWriter(&wr)
+	err = initial.Execute(fmtwr, d)
+	if err != nil {
+		return "", err
+	}
+	return wr.String(), nil
+}
