be846192f38e3b8d8b69d32797a069508a40893b
Nick Klauer <klauer@gmail.com>
2016-11-30 11:51:00 -0600

    formatting and file writing

--- tools.go
+++ tools.go
@@ -1,11 +1,15 @@
 package adr
 
-import "text/template"
-import "time"
-import "io/ioutil"
-
-import "bufio"
-import "bytes"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"text/template"
+	"time"
+)
 
 // DecisionRecord is a type of Architecture Decision Record.
 type DecisionRecord struct {
@@ -18,25 +22,35 @@ type DecisionRecord struct {
 	Consequences string
 }
 
-func (d DecisionRecord) format() (string, error) {
+func (d DecisionRecord) format() (bytes.Buffer, error) {
 	tmpl, err := ioutil.ReadFile("templates/template.md")
 	if err != nil {
-		return "", err
+		return bytes.Buffer{}, err
 	}
 	initial, err := template.New("initial").Parse(string(tmpl))
 	if err != nil {
-		return "", err
+		return bytes.Buffer{}, err
 	}
 
 	var wr bytes.Buffer
 	fmtwr := bufio.NewWriter(&wr)
 	err = initial.Execute(fmtwr, d)
 	if err != nil {
-		return "", err
+		return bytes.Buffer{}, err
 	}
-	return wr.String(), nil
+	return wr, nil
+}
+
+func (d DecisionRecord) formatTitle() string {
+	underscorer := strings.NewReplacer(" ", "_")
+	return fmt.Sprintf("%d-%s", d.Number, underscorer.Replace(d.Title))
 }
 
 func (d DecisionRecord) writeFile(path string) error {
-
+	b, err := d.format()
+	if err != nil {
+		return err
+	}
+	p := filepath.Join(path, d.formatTitle())
+	return ioutil.WriteFile(p, b.Bytes(), 0644)
 }
