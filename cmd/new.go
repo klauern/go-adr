f7fa1550e3920538d630e1644e394b1c68a43e88
Nick Klauer <klauer@gmail.com>
2017-03-30 19:31:10 -0500

    more copyright changes

--- cmd/new.go
+++ cmd/new.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Nick Klauer <klauer@gmail.com>
+// Copyright © 2017 Nick Klauer <klauer@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
